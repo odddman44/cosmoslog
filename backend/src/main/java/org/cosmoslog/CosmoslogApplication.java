dd06f8aa5e541d65042606036644062c1ead353a
odddman44 <odddman44@users.noreply.github.com>
2026-01-09 18:02:36 +0900

    feat: setup tailwind and tiptap editor base layout

--- backend/src/main/java/org/cosmoslog/CosmoslogApplication.java
+++ backend/src/main/java/org/cosmoslog/CosmoslogApplication.java
@@ -1,7 +1,9 @@
 package org.cosmoslog;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class CosmoslogApplication {
 
 	public static void main( String[] args ) {
